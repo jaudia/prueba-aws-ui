0b80cda0c6f20f58b05034b8eb8c484c4c200db5
Juan <juan.audia@gmail.com>
2022-12-08 11:58:08 -0300

    prueba testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Prueba de react</p>
+        <p>Prueba de react - aws</p>
       </header>
     </div>
   );
