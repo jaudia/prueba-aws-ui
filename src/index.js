377489cf858abacc42343754638013bb59629f39
Juan <juan.audia@gmail.com>
2022-12-08 18:09:12 -0300

    app js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
 import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
-import App from './App';
+import App from './App.js';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 const root = document.getElementById('root'); // <- This is the correct method call for React version 17
@@ -11,4 +11,4 @@ render(
     <App />
   </React.StrictMode>,
   root
-);
+); 
