0c60524a19a014889c5557af1908fb842ea306dd
Juan <juan.audia@gmail.com>
2022-12-10 17:57:52 -0300

    cambios package y render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; //<- This import is only for React version 18
-// import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
+// import ReactDOM from 'react-dom/client'; //<- This import is only for React version 18
+import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
 import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// const root = document.getElementById('root'); // <- This is the correct method call for React version 17
-ReactDOM.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
