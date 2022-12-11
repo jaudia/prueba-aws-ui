93e5bb11973b1faebb57b4cf5acdf7faa5ddb8d0
Juan <juan.audia@gmail.com>
2022-12-11 14:48:16 -0300

    env

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client'; //<- This import is only for React version 18
 import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
-import dotenv from 'dotenv';
-dotenv.config();
 import App from './App.js';
+
+
+console.log('---------------------------------------------------------------')
+console.log('---------------------------------------------------------------')
+console.log('---------------------------------------------------------------')
+console.log(process.env);
+
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 const root = document.getElementById('root'); // <- This is the correct method call for React version 17
