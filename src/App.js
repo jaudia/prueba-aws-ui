bbf2ea2529fd2d3330eb4017083e2fc2bff43033
Juan <juan.audia@gmail.com>
2022-12-11 17:04:12 -0300

    modificacion

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  alert(process.env.REACT_APP_OTRA_VARIABLE);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Prueba de react - aws</p>
+        <p>Prueba de react - aws - 2</p>
       </header>
     </div>
   );
