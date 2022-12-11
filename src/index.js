e2b6148bebc8012fd4b97938a242446cbce2d225
Juan <juan.audia@gmail.com>
2022-12-11 14:20:41 -0300

    dotenv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client'; //<- This import is only for React version 18
 import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
+require('dotenv').config();
 import App from './App.js';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
