be9da2d2e8f6798cca7f3475fd80309f1ca9b918
Juan <juan.audia@gmail.com>
2022-12-11 14:29:13 -0300

    dotenv

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client'; //<- This import is only for React version 18
 import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import dotenv from 'dotenv';
 dotenv.config();
 import App from './App.js';
 
